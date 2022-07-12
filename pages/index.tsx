129dffe100918ef23dda2dbb5c67b5fff4c34090
Bruno Silva <brunosilvabrait@gmail.com>
2022-07-12 19:06:03 -0300

    Configuração do projeto, pastas e arquivos e instalação das libs que serão usadas

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Styles from '../styles/HomeNoAuth.module.scss';
+
+const HomeNoAuth = () => {
+   return (
+      <>
+         <Head>   
+            <title>Home</title>
+         </Head>
+         <main>
+
+         </main>
+      </>
+   );
+};
+
+export default HomeNoAuth;
