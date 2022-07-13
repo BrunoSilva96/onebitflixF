69f0ba38f744fb57ee03e781dd60bc4549205791
Bruno Silva <brunosilvabrait@gmail.com>
2022-07-13 15:17:21 -0300

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,16 @@
 import Head from 'next/head'
-import Styles from '../styles/HomeNoAuth.module.scss';
+import styles from '../styles/HomeNoAuth.module.scss';
 
 const HomeNoAuth = () => {
    return (
       <>
          <Head>   
-            <title>Home</title>
+            <title>Onebitflix</title>
+            <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+            <meta property="og:title" content="Onebitflix" key="title"/>
+            <meta name="descripton" content="Tenha acesso aos melhores conteúdos de programação de uma forma simples e fácil!"/>
          </Head>
-         <main>
-
-         </main>
+         <main></main>
       </>
    );
 };
